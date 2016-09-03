f8d79ab358be747f7ebaa19e4b843bf4947733a6
Aakash Gupta <aakashgupta.0205@gmail.com>
2016-09-03 19:08:43 +0800

    notification buttons

--- app/src/main/java/com/example/prjagannath/castus/API/MyFirebaseMessagingService.java
+++ app/src/main/java/com/example/prjagannath/castus/API/MyFirebaseMessagingService.java
@@ -1,15 +1,18 @@
 package com.example.prjagannath.castus.API;
 
+import android.annotation.TargetApi;
+import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.media.RingtoneManager;
 import android.net.Uri;
-import android.support.v4.app.NotificationCompat;
+import android.os.Build;
 import android.util.Log;
 
 import com.example.prjagannath.castus.R;
+import com.example.prjagannath.castus.CustomUI.NotificationButton;
 import com.example.prjagannath.castus.src.PostLoginActivity;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
@@ -65,26 +68,37 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
      *
      * @param messageBody FCM message body received.
      */
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
     private void sendNotification(String messageTitle, String messageBody) {
-        Intent intent = new Intent(this, PostLoginActivity.class);
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
+
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        Intent acceptIntent = new Intent(this, PostLoginActivity.class);
+        Intent rejectIntent = new Intent(this, NotificationButton.class);
+        acceptIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        rejectIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingAcceptIntent = PendingIntent.getActivity(this, 0 /* Request code */, acceptIntent,
+                PendingIntent.FLAG_ONE_SHOT);
+        PendingIntent pendingRejectIntent = PendingIntent.getActivity(this, 1 /* Request code */, rejectIntent,
                 PendingIntent.FLAG_ONE_SHOT);
 
         Uri defaultSoundUri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
-        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
+
+        Notification notifi = new Notification.Builder(this)
                 .setSmallIcon(R.drawable.com_facebook_button_icon_blue)
                 .setContentTitle(messageTitle)
                 .setContentText(messageBody)
                 .setAutoCancel(true)
                 .setSound(defaultSoundUri)
-                .setContentIntent(pendingIntent)
-                .addAction(R.drawable.com_facebook_close, "Close", pendingIntent)
-                .addAction(R.drawable.com_facebook_send_button_icon, "Accept", pendingIntent);
+                .setContentIntent(pendingAcceptIntent)
+                .addAction(R.drawable.ic_reject, "Reject", pendingRejectIntent)
+                .addAction(R.drawable.ic_accept, "Accept", pendingAcceptIntent)
+                .setDeleteIntent(pendingRejectIntent)
+                .build();
 
-        NotificationManager notificationManager =
-                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        notifi.flags |= Notification.FLAG_AUTO_CANCEL;
 
-        notificationManager.notify(0 /* ID of notification */, notificationBuilder.build());
+        notificationManager.notify(0, notifi);
+
     }
 }
